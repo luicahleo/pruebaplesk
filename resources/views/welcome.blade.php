4ef5dc8293ad3ad9f76e8eded745a0a14b2eab77
luicahleo <luisrolandocahuanaleon@gmail.com>
2020-12-07 09:12:22 +0100

    probando nuevo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PruebaPlesk
                 </div>
 
                 <div class="links">
